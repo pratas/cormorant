5ed1c19ed5161715792f2891b3a5f529cea590c4
pratas <pratas@ua.pt>
2018-03-29 13:50:35 +0100

    filter

---
+++ filter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#==============================================================================
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/845/085/GCF_000845085.1_ViralProj14518/GCF_000845085.1_ViralProj14518_genomic.fna.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/857/865/GCF_000857865.1_ViralProj15107/GCF_000857865.1_ViralProj15107_genomic.fna.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/858/645/GCF_000858645.1_ViralProj15113/GCF_000858645.1_ViralProj15113_genomic.fna.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/859/465/GCF_000859465.1_ViralProj15106/GCF_000859465.1_ViralProj15106_genomic.fna.gz
+#==============================================================================
